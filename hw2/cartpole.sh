24e3b5f96eb7f4fdcde700cc5df1c675890f881c
Michael Whittaker <mjwhittaker@berkeley.edu>
2017-09-17 19:01:08 -0700

    Minor script tweaks.

--- hw2/cartpole.sh
+++ hw2/cartpole.sh
@@ -3,14 +3,16 @@
 set -euo pipefail
 
 main() {
+    n=100
+    e=5
     flags="--verbose --n_layers 1 --size 32"
     set -x
-    python train_pg.py CartPole-v0 $flags -n 100 -b 1000 -e 5      -dna --exp_name sb_no_rtg_dna
-    python train_pg.py CartPole-v0 $flags -n 100 -b 1000 -e 5 -rtg -dna --exp_name sb_rtg_dna
-    python train_pg.py CartPole-v0 $flags -n 100 -b 1000 -e 5 -rtg      --exp_name sb_rtg_na
-    python train_pg.py CartPole-v0 $flags -n 100 -b 5000 -e 5      -dna --exp_name lb_no_rtg_dna
-    python train_pg.py CartPole-v0 $flags -n 100 -b 5000 -e 5 -rtg -dna --exp_name lb_rtg_dna
-    python train_pg.py CartPole-v0 $flags -n 100 -b 5000 -e 5 -rtg      --exp_name lb_rtg_na
+    python train_pg.py CartPole-v0 $flags -n $n -b 1000 -e $e      -dna --exp_name sb_no_rtg_dna
+    python train_pg.py CartPole-v0 $flags -n $n -b 1000 -e $e -rtg -dna --exp_name sb_rtg_dna
+    python train_pg.py CartPole-v0 $flags -n $n -b 1000 -e $e -rtg      --exp_name sb_rtg_na
+    python train_pg.py CartPole-v0 $flags -n $n -b 5000 -e $e      -dna --exp_name lb_no_rtg_dna
+    python train_pg.py CartPole-v0 $flags -n $n -b 5000 -e $e -rtg -dna --exp_name lb_rtg_dna
+    python train_pg.py CartPole-v0 $flags -n $n -b 5000 -e $e -rtg      --exp_name lb_rtg_na
     set +x
 }
 
