d21f09977596aff1e15a5322599860c9111edb5e
Michael Whittaker <mjwhittaker@berkeley.edu>
2017-09-20 10:14:11 -0700

    Winning cheetah script.

---
+++ hw2/cheetah.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+main() {
+    fixed_flags="-ep 150 --discount 0.9 -n 100"
+    python train_pg.py \
+        HalfCheetah-v1 \
+        $fixed_flags \
+        --verbose \
+        --n_layers 2 \
+        --size 32 \
+        --seed 3 \
+        -b 50000 \
+        -e 1 \
+        --learning_rate 0.02 \
+        -rtg \
+        --exp_name cheetah
+}
+
+# 0.0175 learning rate; 2 x 16 gets 130 or so pretty consistently
+
+main
